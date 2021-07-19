5a8eae2fc0d36b705d7432070390e3d3108164ed
Ivan Chernetskyi <Ivan.Chernetskyi@medhost.com>
2021-07-19 19:35:25 +0300

    Initial Commit

---
+++ Diia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Diia
+//
+//  Created by Ivan Chernetskyi on 19.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
